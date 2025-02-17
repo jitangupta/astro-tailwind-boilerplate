fed4a27317dd4b49589be7bdc706074116a945a8
jitangupta <gtangupta@gmail.com>
2025-02-17 13:07:32 +0530

    deployment setup for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://jitangupta.github.io',
+  base: 'astro-tailwind-boilerplate',
 });
